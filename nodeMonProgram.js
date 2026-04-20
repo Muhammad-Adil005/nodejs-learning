d5963185661a41b2b5b1d0cb66d2a3a65dcb83f8
Muhammad Adil <64356402+Muhammad-Adil005@users.noreply.github.com>
2026-04-20 16:34:06 +0500

    Nodemon in node Js

---
+++ nodeMonProgram.js
@@ -0,0 +1,7 @@
+console.log('All About NodeMon'); 
+
+// Nodemon package is used to see the result instantly without re Run the App again and again. 
+
+// when You install Nodemon Globally like  npm i nodemon -g   its install nodemon globally but we cannot see this package inside package.json file
+ 
+console.log('This is the Adil Flutter Developer, 2026');
