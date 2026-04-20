8ee5e6c973941fbe8abdffd24a83dc7a5bf5d5c0
Muhammad Adil <64356402+Muhammad-Adil005@users.noreply.github.com>
2026-04-20 16:32:22 +0500

    First program on Node js by just write console.log

---
+++ firstProgram.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+console.log("Muhammad Adil");
+
+// Very Very helpful interview Quesiton
+
+// Question :> Is this console.log and Javascript is same or not?
+
+// Ans :> No. Its not the Same. This console.log is part of the node js module. It come from Node Module.  The console.log in JS is part of the Web Browser. so thats the differene b/w Console.log in node js and Javascript
+
+var a = 10;
+
+for (var a = 0; a <=10; a++){
+    console.log(`The Output will be ${a}`);
+}
+
+let num1 = 100;
+
+for(let num1 = 10; num1 <= 100; num1++){
+    console.log(`The Output of Number 1 will be == ${num1}`);
+}
