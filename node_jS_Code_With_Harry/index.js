f9464cdf539933b978456d84c6b84bc93b27e6df
Muhammad Adil <64356402+Muhammad-Adil005@users.noreply.github.com>
2026-04-23 10:29:25 +0500

    addings node js code files for Code with Harry

---
+++ node_jS_Code_With_Harry/index.js
@@ -0,0 +1,9 @@
+const lovish = require("./second");
+
+// const additionOfTwoNumbers = lovish(10, 20);
+// console.log(additionOfTwoNumbers);
+
+console.log("Hello world", lovish) // This will print the Object harry after the text Hello World
+console.log('I am Flutter Developer');
+console.log("I Love Landikotal");
+console.log("I Love Good! and my Health is also Good Alhamdolelah");
---
+++ node_jS_Code_With_Harry/second.js
@@ -0,0 +1,18 @@
+// function add( a,  b){
+//     return a + b;
+// }
+
+
+// module.exports = add;
+
+
+harry = {
+    name: "Harry",
+    favNum: 36,
+    developer: true
+}
+module.exports = harry;
+
+// console.log(exports, require, module, __filename, __dirname)
+    
+// })
