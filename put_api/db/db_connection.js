c6588dc61ff7dc2b3fd21694c71440bca7e2803d
Muhammad Adil <64356402+Muhammad-Adil005@users.noreply.github.com>
2026-04-28 15:30:13 +0500

    put api code in node js

---
+++ put_api/db/db_connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(''); // Your Connection String Goes Here
+        console.log("Mongo DB Connected Successfully")
+    }
+    catch (err) {
+        console.log("Mongo DB Connection Error: ", err);
+    }
+}
+
+module.exports = connectDB;
